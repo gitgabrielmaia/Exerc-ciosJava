8dcbcef4393c105ce70f8103c43385dfb9bddc96
gitgabrielmaia <gitgabrielmaia@gmail.com>
2025-01-25 21:40:08 -0300

    ex01-Calculadora

---
+++ calculadoraSimples/Calculadora.java
@@ -0,0 +1,51 @@
+package calculadoraSimples;
+
+import  java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {    
+            System.out.println("1. Soma");
+            System.out.println("2. Subtração");
+            System.out.println("3. Multiplicação");
+            System.out.println("4. Divisão");
+            System.out.print("Digite o número da operação: ");
+            int operacao = scanner.nextInt();
+
+            System.out.print("Digite o primeiro número: ");
+            double num1 = scanner.nextDouble();
+
+            System.out.print("Digite o segundo número: ");
+            double num2 = scanner.nextDouble();
+
+            double resultado = 0;
+
+            switch (operacao) {
+                case 1:
+                    resultado = num1 + num2;
+                    System.out.println("Resultado: " + resultado);
+                    break;
+                case 2:
+                    resultado = num1 - num2;
+                    System.out.println("Resultado: " + resultado);
+                    break;
+                case 3:
+                    resultado = num1 * num2;
+                    System.out.println("Resultado: " + resultado);
+                    break;
+                case 4:
+                    if (num2 != 0) {
+                        resultado = num1 / num2;
+                        System.out.println("Resultado: " + resultado);
+                    } else {
+                        System.out.println("Erro: Divisão por zero!");
+                    }
+                    break;
+                default:
+                    System.out.println("Operação inválida!");
+                    break;
+            }
+        }
+      
+    }
+}
